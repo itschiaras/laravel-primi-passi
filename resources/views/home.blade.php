7a58a72234e9ee72cc87096639d30fd56cf9a3a1
ChiaraS97 <chiara.sucato97@gmail.com>
2023-05-19 16:32:14 +0200

    ex. complete + bonus

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar p-3">
+                <ul class="d-flex list-unstyled gap-3">
+                    <li><a href="{{ route('home')}} ">Home</a></li>
+                    <li><a href="{{ route('contacts')}} ">Contatti</a></li>
+                    <li><a href="{{ route('aboutus')}} ">About us</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="pt-3">
+            <h1 class="text-center">{{$title}}</h1>
+        </main>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'title' => 'Primi Passi su Laravel :D',
+        'navlinks' =>
+         [
+            'home' => 'Home',
+            'about' =>'About Us',
+            'contacts' => 'Contact Us'
+         ]
+    ];
+    return view('home', $data);
+})->name('home');
+
+Route::get('/contatti', function () {
+    $data = [
+        'page_title' => 'Contattami qui:',
+        'email' => 'chiara_97_@gmail.com'
+
+    ];
+    return view('contacts', $data);
+})->name('contacts');
+
+Route::get('/aboutus', function () {
+    $data = [
+        'name_surname' => 'Chiara Sucato',
+        'info' => 'Studente Boolean Classe 92!!'
+
+    ];
+    return view('aboutus', $data);
+})->name('aboutus');
